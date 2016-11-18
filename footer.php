9c2e9419c89d50191ac7941923aac7c43641c9fe
hc692 <hc692@cornell.edu>
2016-11-18 18:55:44 -0500

    index, footer, css

---
+++ footer.php
@@ -0,0 +1,29 @@
+<?php
+    
+echo '
+
+    
+    <div id="footer">
+        <p id="footer_words">Stay up to date and follow Apple Fest on social media!</p>
+
+        <div id="social_media">
+
+            <!--facebook-->
+            <a href="http://www.facebook.com/sharer.php?u=https://simplesharebuttons.com" target="_blank">
+            <img src="https://simplesharebuttons.com/images/somacro/facebook.png" alt="Facebook" />
+            </a>
+
+            <!-- Twitter -->
+            <a href="https://twitter.com/share?url=https://simplesharebuttons.com&amp;text=Simple%20Share%20Buttons&amp;hashtags=simplesharebuttons" target="_blank">
+            <img src="https://simplesharebuttons.com/images/somacro/twitter.png" alt="Twitter" />
+            </a>
+
+            <!--social media code adapted from https://simplesharebuttons.com/html-share-buttons/ -->
+
+        </div>
+
+    </div>
+    
+    ';
+?>
+
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Apple Fest Website</title>
+        <link rel="stylesheet" type="text/css" href="styles/styles.css">
+    </head>
+    
+    <body>
+        <div id="include_footer">
+            <?php include "footer.php" ?>
+        </div>
+    </body>
+</html>
