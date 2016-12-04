d7415a371ab78bc3de3c60e96d7569b432e0aaea
Courtney Cheung <courtneycheung@dhcp-mcfad-2687.redrover.cornell.edu>
2016-12-04 17:14:13 -0500

    homepage image/description

--- index.php
+++ index.php
@@ -22,11 +22,19 @@
         </div>
        
         <div id="container">
-            <p>This year's apple fest is taking place in October 2017. Located in Ithaca Commons, the whole Ithaca community looks forward to this annual event where vendors, entertainers, families, citizens, and musicians, all gather for fresh apples and fun. </p>
+            <div id="home_image">
+                <img id="home_pic" src="images/apples_home.jpg" alt="apples">
+                 <!--picture citation: https://guiltlesssplurge.files.wordpress.com/2010/10/dscn0815.jpg-->
+            </div>
             
-            <p>Apple Fest has a large variety of activities you and your family can participate in. There are both friendly-family activities and things for just the parents to do while the kids play on the playground in the Commons. Some activities that your kids will love are face painting, apple tasting, coloring apple-related pages. For a comprehensie list for you and your family, please refer to the "Schedule" page.</p>
+            <p> Welcome to the Ithaca Apple Fest Website! This year marks the 35th Annual Ithaca Apple Harvest Festival presented by the Tompkins Trust Company. Apple Festival hosts a variety of locally grown apples as well as other produce, fresh baked goods, games, live entertainment and more!</p>
             
-            <p> Courtney </p>
+            <p>Spend a family weekend at Apple Fest and participate in the fun! Apple Fest has a large variety of activities you and your family can participate in. There are friendly-family activities for both the parents and children as well as  vendor booths for the adults. Some activities that your kids will love are face painting, apple tasting, coloring apple-related pages, etc. For a comprehensive list of activities and events, please refer to the Schedule page.</p>
+            
+            <p> Along with activities for the whole family, Apple Fest will have a variety of vendors that cater to all different tastes. There will be delicious fresh fruit, apple pies, kettlecorn, caramel apples, and everyone's all-time favorite: hot apple cider donuts!</p>
+            
+            <p> When: October 2017 </p>
+            <p> Where: Ithaca Commons, 171 E M.L.K. Jr. St, Ithaca, NY 14850</p>
             
         </div>
         
