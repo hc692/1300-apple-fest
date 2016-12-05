5b5f3cedc59141dfce5e337494dd8469c7559a55
Courtney Cheung <courtneycheung@dhcp-mcfad-2687.redrover.cornell.edu>
2016-12-04 22:40:23 -0500

    homepage

--- index.php
+++ index.php
@@ -26,9 +26,8 @@
             </p>
         <div id="container">
 
-            <img id="home_pic" src="images/apples_home.jpg" alt="apples">
+            <img id="home_pic" src="images/apples_home2.jpg" alt="apples" class="home">
                 <!--picture citation: https://guiltlesssplurge.files.wordpress.com/2010/10/dscn0815.jpg-->
-
             
             <p>
                 This year marks the <span id="annual"> 35th Annual Ithaca Apple Harvest Festival </span> presented by the Tompkins Trust Company. Apple Festival hosts a variety of locally grown apples as well as other produce, fresh baked goods, games, live entertainment and more!
@@ -40,15 +39,31 @@
             <p>Some activities that your kids will love are face painting, apple tasting, coloring apple-related pages, etc. For a comprehensive list of activities and events, please refer to the Schedule page.
             </p>
             
-            <p> Along with activities for the whole family, Apple Fest will have a variety of vendors that cater to all different tastes. There will be delicious fresh fruit, apple pies, kettlecorn, caramel apples, and everyone's all-time favorite: hot apple cider donuts!</p>
             
-            <p> 
-                <span id="when">When: </span> October 2017 
+            <p class="food">
+                Along with activities for the whole family, Apple Fest will have a variety of vendors that cater to all different tastes. There will be delicious fresh fruit, apple pies, kettlecorn, caramel apples, and everyone's all-time favorite: hot apple cider donuts!</p>
+            
+            <div id="memo">
                 
-            </p>
-            <p> 
-               <span id="where">Where:</span> Ithaca Commons, 171 E M.L.K. Jr. St, Ithaca, NY 14850 
-            </p>
+                <img id="caramel_pic" src="images/caramel_apples.jpeg" alt="caramel" class="caramel">
+                <!--picture citation: http://www.hercampus.com/school/ithaca/best-apple-fest-->
+                
+                <div id ="date">
+                <p class="save">
+                    Save the Date!
+
+                </p>
+                <p> 
+                    <span id="when">When: </span> October 2017 
+
+                </p>
+                <p> 
+                   <span id="where">Where:</span> Ithaca Commons, 171 E M.L.K. Jr. St, Ithaca, NY 14850 
+                </p>
+                <p> 
+                    <span id="cost">Cost:</span> FREE</p>
+                </div>
+            </div>
             
         </div>
             
