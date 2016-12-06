da857d2d3dfe9740748cd95a2122e09df2f2c2d1
hc692 <hc692@cornell.edu>
2016-12-06 14:19:13 -0500

    centered schedules

--- schedule.php
+++ schedule.php
@@ -19,108 +19,108 @@
         <div id="container">
             <div id="head-block">
             
-            <h2>Planning your visit?</h2>
-            <p>Here is everything you need to know about the schedule for this year's festival,
-            all in one place.</p>
-            <h4>Festival Dates: Friday September 30, 2016 - Sunday October 2, 2016</h4>
-            <img src="images/neo_project.jpg" alt="NEO Project">
-            <!-- image source: https://www.gigsalad.com/neo_project_ithaca -->
+                <h2>Planning your visit?</h2>
+                <p>Here is everything you need to know about the schedule for this year's festival,
+                all in one place.</p>
+                <h4>Festival Dates: Friday September 30, 2016 - Sunday October 2, 2016</h4>
+                <img src="images/neo_project.jpg" alt="NEO Project">
+                <!-- image source: https://www.gigsalad.com/neo_project_ithaca -->
             </div>
 
             <div class="schedule-block">
-            <h3>All Performances at the Bernie Milton Pavilion (Main Stage)</h3>
-            <table class="times">
-                <tr>
-                    <th>Performer</th>
-                    <th>Day</th>
-                    <th>Time</th>
-                </tr>
-                <tr>
-                    <td>Punk Hands</td>
-                    <td>Saturday</td>
-                    <td>10:00-10:45am</td>
-                </tr>
-                <tr>
-                    <td>Danza Romani</td>
-                    <td>Saturday</td>
-                    <td>11:00-11:45am</td>
-                </tr>
-                <tr>
-                    <td>Vitamin L</td>
-                    <td>Saturday</td>
-                    <td>12:00-12:45pm</td>
-                </tr>
-                <tr>
-                    <td>Riley Owens</td>
-                    <td>Saturday</td>
-                    <td>1:00-1:45pm</td>
-                </tr>
-                <tr>
-                    <td>GIVAWAYS</td>
-                    <td>Saturday</td>
-                    <td>1:45-2:00pm</td>
-                </tr>
-                <tr>
-                    <td>Sunny Weather</td>
-                    <td>Saturday</td>
-                    <td>2:00-2:45pm</td>
-                </tr>
-                <tr>
-                    <td>Eems</td>
-                    <td>Saturday</td>
-                    <td>3:00-3:45pm</td>
-                </tr>
-                <tr>
-                    <td>Alter</td>
-                    <td>Saturday</td>
-                    <td>4:00-4:45pm</td>
-                </tr>
-                <tr>
-                    <td>Wailin' Brass Band</td>
-                    <td>Saturday</td>
-                    <td>5:00-6:00pm</td>
-                </tr>
-                <tr>
-                    <td>IC Voicestream, Ithacapella, Premium Blend</td>
-                    <td>Sunday</td>
-                    <td>10:00-10:45am</td>
-                </tr>
-                <tr>
-                    <td>Sixteen Feathers</td>
-                    <td>Sunday</td>
-                    <td>11:00-11:45am</td>
-                </tr>
-                <tr>
-                    <td>June w/ Chandani</td>
-                    <td>Sunday</td>
-                    <td>12:00-12:45pm</td>
-                </tr>
-                <tr>
-                    <td>Mary Lorson</td>
-                    <td>Sunday</td>
-                    <td>1:00-1:45pm</td>
-                </tr>
-                <tr>
-                    <td>Dapper Dan</td>
-                    <td>Sunday</td>
-                    <td>2:00-3:00pm</td>
-                </tr>
-                <tr>
-                    <td>Radio London</td>
-                    <td>Sunday</td>
-                    <td>3:15-4:15pm</td>
-                </tr>
-                <tr>
-                    <td>Mirage Belly Dancers</td>
-                    <td>Sunday</td>
-                    <td>4:30-5:00pm</td>
-                </tr>
-                <tr>
-                    <td>NEO Project</td>
-                    <td>Sunday</td>
-                    <td>5:15-6:15pm</td>
-                </tr>
-            </table>
+                <h3>All Performances at the Bernie Milton Pavilion (Main Stage)</h3>
+                <table class="times">
+                    <tr>
+                        <th>Performer</th>
+                        <th>Day</th>
+                        <th>Time</th>
+                    </tr>
+                    <tr>
+                        <td>Punk Hands</td>
+                        <td>Saturday</td>
+                        <td>10:00-10:45am</td>
+                    </tr>
+                    <tr>
+                        <td>Danza Romani</td>
+                        <td>Saturday</td>
+                        <td>11:00-11:45am</td>
+                    </tr>
+                    <tr>
+                        <td>Vitamin L</td>
+                        <td>Saturday</td>
+                        <td>12:00-12:45pm</td>
+                    </tr>
+                    <tr>
+                        <td>Riley Owens</td>
+                        <td>Saturday</td>
+                        <td>1:00-1:45pm</td>
+                    </tr>
+                    <tr>
+                        <td>GIVAWAYS</td>
+                        <td>Saturday</td>
+                        <td>1:45-2:00pm</td>
+                    </tr>
+                    <tr>
+                        <td>Sunny Weather</td>
+                        <td>Saturday</td>
+                        <td>2:00-2:45pm</td>
+                    </tr>
+                    <tr>
+                        <td>Eems</td>
+                        <td>Saturday</td>
+                        <td>3:00-3:45pm</td>
+                    </tr>
+                    <tr>
+                        <td>Alter</td>
+                        <td>Saturday</td>
+                        <td>4:00-4:45pm</td>
+                    </tr>
+                    <tr>
+                        <td>Wailin' Brass Band</td>
+                        <td>Saturday</td>
+                        <td>5:00-6:00pm</td>
+                    </tr>
+                    <tr>
+                        <td>IC Voicestream, Ithacapella, Premium Blend</td>
+                        <td>Sunday</td>
+                        <td>10:00-10:45am</td>
+                    </tr>
+                    <tr>
+                        <td>Sixteen Feathers</td>
+                        <td>Sunday</td>
+                        <td>11:00-11:45am</td>
+                    </tr>
+                    <tr>
+                        <td>June w/ Chandani</td>
+                        <td>Sunday</td>
+                        <td>12:00-12:45pm</td>
+                    </tr>
+                    <tr>
+                        <td>Mary Lorson</td>
+                        <td>Sunday</td>
+                        <td>1:00-1:45pm</td>
+                    </tr>
+                    <tr>
+                        <td>Dapper Dan</td>
+                        <td>Sunday</td>
+                        <td>2:00-3:00pm</td>
+                    </tr>
+                    <tr>
+                        <td>Radio London</td>
+                        <td>Sunday</td>
+                        <td>3:15-4:15pm</td>
+                    </tr>
+                    <tr>
+                        <td>Mirage Belly Dancers</td>
+                        <td>Sunday</td>
+                        <td>4:30-5:00pm</td>
+                    </tr>
+                    <tr>
+                        <td>NEO Project</td>
+                        <td>Sunday</td>
+                        <td>5:15-6:15pm</td>
+                    </tr>
+                </table>
             </div>
             
             <div class="schedule-block">
