d31bf99f5a7dad3cae378e4ab1481275aa02b3c9
Paul DeVito <pid7@cornell.edu>
2016-12-06 14:25:15 -0500

    images change back

--- js/main.js
+++ js/main.js
@@ -1,17 +1,35 @@
 //jQuery file for Apple Fest 
 
+//global variables
+var kid_stage = true;
+var parent_stage = true;
+
 $(document).ready(function() {
+    
     
     //define functions
     
     //define AND call function
     $('#vendors_kids').click(function(){
-        $('#vendors_kids').attr('src', 'images/green.PNG');
+        if (kid_stage) {
+            $('#vendors_kids').attr('src', 'images/green.PNG');
+            kid_stage = false;
+            
+        } else {
+            $('#vendors_kids').attr('src', 'images/face_painting.jpg');
+            kid_stage = true;
+        }
     });
 
     
     $('#vendors_parents').click(function(){
-        $('#vendors_parents').attr('src', 'images/red.PNG');
+        if (parent_stage) {            
+            $('#vendors_parents').attr('src', 'images/red.PNG');
+            parent_stage = false;
+        } else {
+            $('#vendors_parents').attr('src', 'images/IMG_6118.jpg');
+            parent_stage = true;
+        }
     });
     
     //call
