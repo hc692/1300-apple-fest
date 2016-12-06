fd2b2bdc3257606b250bbde3fb06c79b21bbb884
Paul DeVito <pid7@cornell.edu>
2016-12-05 22:52:25 -0500

    created content for schedule.php
    also added a small doc with notes as a reference for tomorrow's meeting

--- schedule.php
+++ schedule.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <title>Apple Fest Schedule Tab</title>
         <link rel="stylesheet" type="text/css" href="styles/styles.css">
-        
+        <link rel="stylesheet" type="text/css" href="styles/styles_schedule.css">
         <!--reference header font -->
         <link href="https://fonts.googleapis.com/css?family=Lobster+Two" rel="stylesheet">
         
@@ -17,8 +17,160 @@
         </div>
        
         <div id="container">
-            <p>This is the Apple Fest schedule.</p>
+            <div id="head-block">
+            
+            <h2>Planning your visit?</h2>
+            <p>Here is everything you need to know about the schedule for this year's festival,
+            all in one place.</p>
+            <h4>Festival Dates: Friday September 30, 2016 - Sunday October 2, 2016</h4>
+            <img src="images/neo_project.jpg" alt="NEO Project">
+            <!-- image source: https://www.gigsalad.com/neo_project_ithaca -->
+            </div>
+
+            <div class="schedule-block">
+            <h3>All Performances at the Bernie Milton Pavilion (Main Stage)</h3>
+            <table class="times">
+                <tr>
+                    <th>Performer</th>
+                    <th>Day</th>
+                    <th>Time</th>
+                </tr>
+                <tr>
+                    <td>Punk Hands</td>
+                    <td>Saturday</td>
+                    <td>10:00-10:45am</td>
+                </tr>
+                <tr>
+                    <td>Danza Romani</td>
+                    <td>Saturday</td>
+                    <td>11:00-11:45am</td>
+                </tr>
+                <tr>
+                    <td>Vitamin L</td>
+                    <td>Saturday</td>
+                    <td>12:00-12:45pm</td>
+                </tr>
+                <tr>
+                    <td>Riley Owens</td>
+                    <td>Saturday</td>
+                    <td>1:00-1:45pm</td>
+                </tr>
+                <tr>
+                    <td>GIVAWAYS</td>
+                    <td>Saturday</td>
+                    <td>1:45-2:00pm</td>
+                </tr>
+                <tr>
+                    <td>Sunny Weather</td>
+                    <td>Saturday</td>
+                    <td>2:00-2:45pm</td>
+                </tr>
+                <tr>
+                    <td>Eems</td>
+                    <td>Saturday</td>
+                    <td>3:00-3:45pm</td>
+                </tr>
+                <tr>
+                    <td>Alter</td>
+                    <td>Saturday</td>
+                    <td>4:00-4:45pm</td>
+                </tr>
+                <tr>
+                    <td>Wailin' Brass Band</td>
+                    <td>Saturday</td>
+                    <td>5:00-6:00pm</td>
+                </tr>
+                <tr>
+                    <td>IC Voicestream, Ithacapella, Premium Blend</td>
+                    <td>Sunday</td>
+                    <td>10:00-10:45am</td>
+                </tr>
+                <tr>
+                    <td>Sixteen Feathers</td>
+                    <td>Sunday</td>
+                    <td>11:00-11:45am</td>
+                </tr>
+                <tr>
+                    <td>June w/ Chandani</td>
+                    <td>Sunday</td>
+                    <td>12:00-12:45pm</td>
+                </tr>
+                <tr>
+                    <td>Mary Lorson</td>
+                    <td>Sunday</td>
+                    <td>1:00-1:45pm</td>
+                </tr>
+                <tr>
+                    <td>Dapper Dan</td>
+                    <td>Sunday</td>
+                    <td>2:00-3:00pm</td>
+                </tr>
+                <tr>
+                    <td>Radio London</td>
+                    <td>Sunday</td>
+                    <td>3:15-4:15pm</td>
+                </tr>
+                <tr>
+                    <td>Mirage Belly Dancers</td>
+                    <td>Sunday</td>
+                    <td>4:30-5:00pm</td>
+                </tr>
+                <tr>
+                    <td>NEO Project</td>
+                    <td>Sunday</td>
+                    <td>5:15-6:15pm</td>
+                </tr>
+            </table>
+            </div>
+            
+            <div class="schedule-block">
+            <h3>All Performances on West State Street</h3>
+            <table class="times">
+                <tr>
+                    <th>Performer</th>
+                    <th>Day</th>
+                    <th>Time</th>
+                </tr>
+                <tr>
+                    <td>Hilby The Skinny German Juggle Boy</td>
+                    <td>Friday</td>
+                    <td>12:30 and 4:45pm</td>
+                </tr>
+                <tr>
+                    <td>Zajal the Sugarplum Fairy</td>
+                    <td>Saturday</td>
+                    <td>12:00pm</td>
+                </tr>
+                <tr>
+                    <td>The Next Jenneration</td>
+                    <td>Saturday</td>
+                    <td>1:00pm</td>
+                </tr>
+                <tr>
+                    <td>The Strong Woman Show</td>
+                    <td>Saturday</td>
+                    <td>3:00pm</td>
+                </tr>
+                <tr>
+                    <td>IC Circus</td>
+                    <td>Saturday</td>
+                    <td>4:30pm</td>
+                </tr>
+                <tr>
+                    <td>Matan Presberg - Juggler</td>
+                    <td>Sunday</td>
+                    <td>12:30pm</td>
+                </tr>
+                <tr>
+                    <td>Hula Hut Polynesian Dance</td>
+                    <td>Sundayy</td>
+                    <td>3:00pm</td>
+                </tr>
+            </table>
+            </div>
         </div>
+        
+        <!-- schedule content from http://www.downtownithaca.com/ithaca-events/Apple%20Harvest%20Festival%20Presented%20by%20Tompkins%20Trust -->
         
         <div id="include_footer">
             <?php include "scripts/footer.php" ;?>
